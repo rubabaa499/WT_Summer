61039db49548eefd8865e5b1e37d576d21cd3f02
Rubaba Rahman <89207252+rubabaa499@users.noreply.github.com>
2022-05-30 23:30:12 +0600

    Update Calcu.php

---
+++ Lab1/View/Calcu.php
@@ -0,0 +1,26 @@
+<html>
+    <body>
+        
+        <form action="../Control/process.php" method="post" >
+           Enter 1st Number : <input type="number" name="num1">
+           Enter 2nd Number : <input type="number" name="num2">
+
+
+           <h4>Select an Option?</h4>
+
+          
+
+               <input type="radio" name="Calculator" value="+" >+
+               <br>
+               <input type="radio" name="Calculator" value="-" >-
+               <br>
+               <input type="radio" name="Calculator" value="*" >*
+               <br>
+               <input type="radio" name="Calculator" value="/" >/
+               <br>
+               
+          
+           <input type="submit" value="Result">
+        </form>
+    </body>
+</html>
